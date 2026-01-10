f870474c4ef5ed774d9b5142629990dced8cadbf
rudd <rudolftison5@gmail.com>
2026-01-10 16:58:00 +0100

    moved transformation to a seperate module

--- Python/main.py
+++ Python/main.py
@@ -1,28 +1,6 @@
 import cv2 as cv
 import connect as cnt
-import numpy as np
-
-def video_grid(frame_v: np.ndarray,
-               canny_v: np.ndarray,
-               roi_v: np.ndarray,
-               hough_v: np.ndarray) -> np.ndarray:
-    h, w = frame_v.shape[:2]
-
-    frame_v = cv.resize(frame_v, (w, h))
-    canny_v = cv.resize(canny_v, (w, h))
-    roi_v = cv.resize(roi_v, (w, h))
-    hough_v = cv.resize(hough_v, (w, h))
-
-    canny_v = cv.cvtColor(canny_v, cv.COLOR_GRAY2BGR)
-    roi_v = cv.cvtColor(roi_v, cv.COLOR_GRAY2BGR)
-
-    top = np.hstack((frame_v, canny_v))
-    bot = np.hstack((roi_v, hough_v))
-
-    stacked = np.vstack((top, bot))
-
-    return stacked
-
+import transform as tf
 
 cap = cv.VideoCapture(cnt.CAM_URL)
 if not cap.isOpened():
@@ -38,41 +16,14 @@ while True:
 
     frame = cv.resize(frame, (320, 240))
 
-    canny = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    canny = cv.GaussianBlur(canny, (5, 5), 0)
-    canny = cv.Canny(canny, 100, 200)
-
-    height, width = canny.shape
-    mask = np.zeros_like(canny)
-    polygon = np.array([[
-        (0, height),
-        (width, height),
-        (width//2, height//2)
-    ]], np.int32)
-    cv.fillPoly(mask, polygon, 255)
-    roi = cv.bitwise_and(canny, mask)
-
-    lines = cv.HoughLinesP(
-        roi,
-        rho=1,
-        theta=np.pi/180,
-        threshold=50,
-        minLineLength=30,
-        maxLineGap=200
-    )
-    line_frame = np.zeros_like(frame)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            slope = (y2 - y1) / (x2 - x1 + 1e-6)
-            if abs(slope) > 0.5:
-                cv.line(line_frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-    hough = cv.addWeighted(frame, 0.8, line_frame, 1, 1)
+    canny = tf.canny_transform(frame)
+    roi = tf.define_roi(canny)
+    hough = tf.hough_transform(frame, roi)
 
     #cnt.sendUDP(direction)
     #cv.putText(frame, direction, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    stack = video_grid(frame, canny, roi, hough)
+    stack = tf.video_grid(frame, canny, roi, hough)
     cv.imshow("Lane detection", stack)
 
     if cv.waitKey(1) & 0xFF == 27:  # ESC
---
+++ Python/transform.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2 as cv
+
+def video_grid(frame: np.ndarray,
+               canny: np.ndarray,
+               roi: np.ndarray,
+               hough: np.ndarray) -> np.ndarray:
+    height, width = frame.shape[:2]
+
+    frame = cv.resize(frame, (width, height))
+    canny = cv.resize(canny, (width, height))
+    roi = cv.resize(roi, (width, height))
+    hough = cv.resize(hough, (width, height))
+
+    canny = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
+    roi = cv.cvtColor(roi, cv.COLOR_GRAY2BGR)
+
+    top = np.hstack((frame, canny))
+    bot = np.hstack((roi, hough))
+
+    stacked = np.vstack((top, bot))
+
+    return stacked
+
+def canny_transform(frame: np.ndarray) -> np.ndarray:
+    canny = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    canny = cv.GaussianBlur(canny, (5, 5), 0)
+    canny = cv.Canny(canny, 100, 200)
+
+    return canny
+
+def define_roi(frame: np.ndarray) -> np.ndarray:
+    height, width = frame.shape
+    mask = np.zeros_like(frame)
+
+    polygon = np.array([[
+        (0, height),
+        (width, height),
+        (width // 2, height // 2)
+    ]], np.int32)
+
+    cv.fillPoly(mask, polygon, 255)
+    roi = cv.bitwise_and(frame, mask)
+
+    return roi
+
+def hough_transform(frame: np.ndarray, roi: np.ndarray) -> np.ndarray:
+    lines = cv.HoughLinesP(
+        roi,
+        rho=1,
+        theta=np.pi / 180,
+        threshold=50,
+        minLineLength=30,
+        maxLineGap=200
+    )
+
+    line_frame = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            slope = (y2 - y1) / (x2 - x1 + 1e-6)
+            if abs(slope) > 0.5:
+                cv.line(line_frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    hough = cv.addWeighted(frame, 0.8, line_frame, 1, 1)
+
+    return hough
